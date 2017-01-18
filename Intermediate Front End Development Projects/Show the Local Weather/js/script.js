5b87dc1209f402d80df15be70e3e1ab30c0ad5b9
andrei <andrei.ticala@gmail.com>
2017-01-17 19:04:02 -0500

    units fix

--- Intermediate Front End Development Projects/Show the Local Weather/js/script.js
+++ Intermediate Front End Development Projects/Show the Local Weather/js/script.js
@@ -2,10 +2,9 @@
  * Created by AndreiTic on 17/01/17.
  */
 $(document).ready(function () {
-    var longitude;
-    var latitude;
-    var units = 'metric';
 
+    var units = 'metric';
+    getWeather(units);
     $('#toggle-units').on('click', function () {
             var unitsBtn = $('#toggle-units');
             units = unitsBtn.attr('data-toggle');
@@ -14,8 +13,13 @@ $(document).ready(function () {
             } else {
                 unitsBtn.attr('data-toggle', 'imperial').html('Fahrenheit');
             }
+            getWeather(unitsBtn.attr('data-toggle'));
         }
     );
+});
+function getWeather(units) {
+    var longitude;
+    var latitude;
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function (position) {
@@ -29,21 +33,23 @@ $(document).ready(function () {
                 '&appid=5d8e878ce4229f91fb1d45d154cde7a2';
 
             $.getJSON(url, function (data) {
-                $('#wi').addClass('wi-owm-'+data.weather[0].id);
+                var unit;
+                (units == 'metric')? unit = 'celsius' :  unit = 'fahrenheit';
+
+                $('#wi').addClass('wi-owm-' + data.weather[0].id);
                 $('#city').append(data.name);
-                $('#temp').prepend(data.main.temp);
-                $('#temp_min').prepend(data.main.temp_min);
-                $('#temp_max').prepend(data.main.temp_max);
+                $('#temp').html(data.main.temp+'<i class="wi wi-'+unit+'">');
+                $('#temp_min').html(data.main.temp_min+'<i class="wi wi-'+unit+'">');
+                $('#temp_max').html(data.main.temp_max+'<i class="wi wi-'+unit+'">');
+
                 $('#cloudiness').append(data.clouds.all);
                 $('#wind_speed').append(data.wind.speed);
-                $('#wind_direction').prepend(data.wind.deg+'° ');
-                $('#wind_deg').addClass('towards-'+data.wind.deg+'-deg');
+                $('#wind_direction').prepend(data.wind.deg + '° ');
+                $('#wind_deg').addClass('towards-' + data.wind.deg + '-deg');
 
             })
         });
     } else {
         alert("Geolocation is not supported by this browser.");
     }
-
-
-});
+}
