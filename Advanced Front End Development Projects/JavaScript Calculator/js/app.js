91ff911d8fb403490ee2e3976411ac53e665cc79
andreiTic <andrei.ticala@gmail.com>
2017-02-03 17:14:16 -0500

    Calculator JS

--- Advanced Front End Development Projects/JavaScript Calculator/js/app.js
+++ Advanced Front End Development Projects/JavaScript Calculator/js/app.js
@@ -1,9 +1,30 @@
 /**
  * Created by Andrei Ticala on 2017-02-01.
  */
-
 $(document).ready(function () {
-    $('button').on('click',function () {
+    var calc = [];
+
+    $('.number').on('click', function () {
         $('.screen-calc p').append(this.value);
+        calc.push(this.value);
+        console.log(calc.toString());
     });
+
+    $('.equals-btn').on('click', function () {
+        doMath();
+    });
+
+    $('#allClear').on('click', function () {
+        calc = [];
+        $('.screen-calc p').html('');
+    });
+
+    $('.operator').on('click', function () {
+
+    });
+
 });
+
+function doMath() {
+    alert('a');
+}
